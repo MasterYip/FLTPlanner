3b3445efbc06d0a4ca09afdc0fe52c23cfe303f3
RaymonYip-NUC11 <2205929492@qq.com>
2023-11-02 17:13:06 +0800

    refactor: file sturcture

---
+++ fast_legged_planner_py/robot_interface/base_robotinterface.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+'''
+Author: RaymonYip-NUC11 2205929492@qq.com
+Date: 2023-11-02 15:47:47
+LastEditors: RaymonYip-NUC11
+LastEditTime: 2023-11-02 16:52:50
+FilePath: /Fast-Legged-Planner-Test/fast_legged_planner_py/robot_interface/base_robotinterface.py
+Description: file content
+'''
+from abc import abstractmethod, ABCMeta
+
+class Base_RobotInterface(metaclass=ABCMeta):
+    
