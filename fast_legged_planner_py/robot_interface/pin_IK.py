86b8f8d4efe832a356cd20d42db83fd9b5b64e35
RaymonYip-NUC11 <2205929492@qq.com>
2023-11-10 17:45:09 +0800

    feat: pin_IK implement 3D & 6D tasks

--- fast_legged_planner_py/robot_interface/pin_IK.py
+++ fast_legged_planner_py/robot_interface/pin_IK.py
@@ -2,7 +2,7 @@
 Author: RaymonYip-NUC11 2205929492@qq.com
 Date: 2023-11-10 14:55:39
 LastEditors: RaymonYip-NUC11
-LastEditTime: 2023-11-10 15:57:26
+LastEditTime: 2023-11-10 17:44:36
 FilePath: /flplanner_ws/src/fast_legged_planner/fast_legged_planner_py/robot_interface/pin_IK.py
 Description: file content
 '''
@@ -15,29 +15,32 @@ import pinocchio as pin
 
 
 IK_default_settings = {
-    'eps': 1e-4,
-    'IT_MAX': 1000,
-    'DT': 1e-1,
-    'damp': 1e-12,
+    'eps': 1e-4,        # Precision
+    'IT_MAX': 1000,     # Max iterations
+    'DT': 1e-1,         # Time step
+    'damp': 1e-12,      # Damping
     'verbose': False
 }
 
 
-def pinIK_6D(model, data, oMdes: pin.SE3, q0, id, mode='frame',
+def pinIK(model, data, oMdes: pin.SE3, q0, id, id_mode='frame', mode=6,
              IK_settings: dict = IK_default_settings):
     """Inverse kinematics for a 6D target placement
-
+    TODO: Solution contiuity insurement(near singularity)
+    
     Args:
         model (pin.Model): model
         data (pin.Data): data
         oMdes (pin.SE3): desired placement
         q0 (np.ndarray): initial configuration
         id (int): frame id or joint id
-        mode (str, optional): 'frame' or 'joint'. Defaults to 'frame'.
+        id_mode (str, optional): 'frame' or 'joint'. Defaults to 'frame'.
+        mode (int, optional): 6D or 3D. Defaults to 6.
         IK_settings (dict, optional): Defaults to IK_default_settings.
 
     Raises:
-        ValueError: mode must be frame or joint
+        ValueError: id_mode must be frame or joint
+        ValueError: mode must be 6 or 3
 
     Returns:
         np.ndarray: configuration q
@@ -51,93 +54,38 @@ def pinIK_6D(model, data, oMdes: pin.SE3, q0, id, mode='frame',
     DT = IK_settings['DT']
     damp = IK_settings['damp']
     verbose = IK_settings['verbose']
-    if mode != 'frame' and mode != 'joint':
-        raise ValueError('mode must be frame or joint')
+    if id_mode != 'frame' and id_mode != 'joint':
+        raise ValueError('id_mode must be frame or joint')
+    if mode != 6 and mode != 3:
+        raise ValueError('mode must be 6 or 3')
     while True:
         pin.forwardKinematics(model, data, q)
-        if mode == 'frame':
+        if id_mode == 'frame':
             iMd = pin.updateFramePlacement(model, data, id).actInv(oMdes)
         else:
             iMd = data.oMi[id].actInv(oMdes)
-        err = pin.log(iMd).vector  # in (joint) frame
+        if mode == 6:
+            err = pin.log(iMd).vector  # in (joint) frame
+        else:
+            err = pin.log(iMd).vector[:3]  # FIXME: is velocity?
+        
         if norm(err) < eps:
             success = True
             break
         if i >= IT_MAX:
             success = False
             break
-        if mode == 'frame':
+
+        if id_mode == 'frame':
             J = pin.computeFrameJacobian(model, data, q, id)
         else:
-            # in joint frame
-            J = pin.computeJointJacobian(model, data, q, id)
+            J = pin.computeJointJacobian(model, data, q, id)  # in joint frame
         J = -np.dot(pin.Jlog6(iMd.inverse()), J)
-        v = - J.T.dot(solve(J.dot(J.T) + damp * np.eye(6), err))
-        q = pin.integrate(model, q, v*DT)
-        if not i % 10 and verbose:
-            print('%d: error = %s' % (i, err.T))
-        i += 1
-    if verbose:
-        if success:
-            print("Convergence achieved!")
+        if mode == 6:
+            v = - J.T.dot(solve(J.dot(J.T) + damp * np.eye(6), err))
         else:
-            print(
-                "\nWarning: the iterative algorithm has not reached convergence to the desired precision")
-    return q, err, success
-
-
-# FIXME: not implemented
-def pinIK_3D(model, data, trans_des, q0, id, mode='frame',
-             IK_settings: dict = IK_default_settings):
-    """Inverse kinematics for a 3D target placement
-
-    Args:
-        model (pin.Model): model
-        data (pin.Data): data
-        trans_des (np.ndarray): desired position
-        q0 (np.ndarray): initial configuration
-        id (int): frame id or joint id
-        mode (str, optional): 'frame' or 'joint'. Defaults to 'frame'.
-        IK_settings (dict, optional): Defaults to IK_default_settings.
-
-    Raises:
-        ValueError: mode must be frame or joint
-
-    Returns:
-        np.ndarray: configuration q
-        np.ndarray: error
-        bool: success
-    """
-    i = 0
-    q = q0.copy()
-    eps = IK_settings['eps']
-    IT_MAX = IK_settings['IT_MAX']
-    DT = IK_settings['DT']
-    damp = IK_settings['damp']
-    verbose = IK_settings['verbose']
-    if mode != 'frame' and mode != 'joint':
-        raise ValueError('mode must be frame or joint')
-    while True:
-        pin.forwardKinematics(model, data, q)
-        if mode == 'frame':
-            err = trans_des - \
-                pin.updateFramePlacement(model, data, id).translation
-        else:
-            err = trans_des - data.oMi[id].translation
-        if norm(err) < eps:
-            success = True
-            break
-        if i >= IT_MAX:
-            success = False
-            break
-        if mode == 'frame':
-            J = pin.computeFrameJacobian(model, data, q, id)
-        else:
-            # in joint frame
-            J = pin.computeJointJacobian(model, data, q, id)
-        J = -np.dot(pin.Jlog6(iMd.inverse()), J)
-        # v:3 x 1
-        v = - J.T.dot(solve(J.dot(J.T) + damp * np.eye(6), err))
+            J = J[:3, :]
+            v = - J.T.dot(solve(J.dot(J.T) + damp * np.eye(3), err))
         q = pin.integrate(model, q, v*DT)
         if not i % 10 and verbose:
             print('%d: error = %s' % (i, err.T))
--- fast_legged_planner_py/robot_interface/ur5_robotinterface.py
+++ fast_legged_planner_py/robot_interface/ur5_robotinterface.py
@@ -2,13 +2,14 @@
 Author: RaymonYip-NUC11 2205929492@qq.com
 Date: 2023-11-02 17:18:15
 LastEditors: RaymonYip-NUC11
-LastEditTime: 2023-11-10 14:17:36
+LastEditTime: 2023-11-10 17:40:38
 FilePath: /flplanner_ws/src/fast_legged_planner/fast_legged_planner_py/robot_interface/ur5_robotinterface.py
 Description: file content
 '''
 # -*- coding: utf-8 -*-
-from pyexpat import model
+import numpy as np
 from .base_robotinterface import Base_RobotInterface
+from .pin_IK import pinIK
 from ..third_party.meshcat_viewer_wrapper.visualizer import MeshcatVisualizer, colors
 import example_robot_data as robex
 from numpy.linalg import norm
@@ -29,6 +30,13 @@ class UR5_RobotInterface(Base_RobotInterface):
             'collmodel_viz': True
         }
 
+    def pinIK_end3D(self, target, last_q=None):
+        if last_q is None:
+            last_q = self.robot.q0
+        q, error, success = pinIK(
+            self.robot.model, self.robot.data, pin.SE3(np.eye(3), target), last_q, 22, mode=3)
+        return q
+
     def IK_end3D(self, target, last_q=None):
         """Get the target configuration
 
@@ -48,18 +56,28 @@ class UR5_RobotInterface(Base_RobotInterface):
             last_q = self.robot.q0
         return fmin_bfgs(cost, last_q, disp=False, gtol=1e-4)
 
-    def vis_target_reach_3D(self, target, last_q=None):
+    def vis_target_reach_3D(self, target, last_q=None, use_pinIK=False):
         ball_position = list(target)
         ball_rad = 0.04
         ball_id = 'world/ball'
         self.viz.addSphere(ball_id, ball_rad, colors.red_transparent)
         q_ball = ball_position + [1, 0, 0, 0]
         self.viz.applyConfiguration(ball_id, q_ball)
-        cfg = self.IK_end3D(target, last_q)
+        if use_pinIK:
+            cfg = self.pinIK_end3D(target, last_q)
+        else:
+            cfg = self.IK_end3D(target, last_q)
         self.viz.display(cfg)
         if self.settings['collmodel_viz']:
             self.vis_collision_model(cfg)
         return cfg
+
+    def pinIK_end6D(self, SE3target, last_q=None):
+        if last_q is None:
+            last_q = self.robot.q0
+        q, error, success = pinIK(
+            self.robot.model, self.robot.data, SE3target, last_q, 22, mode=6)
+        return q
 
     def IK_end6D(self, SE3target, last_q=None):
         """_summary_
@@ -77,13 +95,16 @@ class UR5_RobotInterface(Base_RobotInterface):
             last_q = self.robot.q0
         return fmin_bfgs(cost, last_q, disp=False, gtol=1e-4)
 
-    def vis_target_reach_6D(self, SE3target, last_q=None):
+    def vis_target_reach_6D(self, SE3target, last_q=None, use_pinIK=False):
         cube_dimension = [0.02, 0.04, 0.06]
         cube_id = 'world/cube'
         self.viz.addBox(cube_id, cube_dimension, colors.red_transparent)
         q_ball = pin.SE3ToXYZQUATtuple(SE3target)
         self.viz.applyConfiguration(cube_id, q_ball)
-        cfg = self.IK_end6D(SE3target, last_q)
+        if use_pinIK:
+            cfg = self.pinIK_end6D(SE3target, last_q)
+        else:
+            cfg = self.IK_end6D(SE3target, last_q)
         self.viz.display(cfg)
         if self.settings['collmodel_viz']:
             self.vis_collision_model(cfg)
