9aea3c9db921649bfbd491c35d651c7105633847
RaymonYip-NUC11 <2205929492@qq.com>
2023-11-13 15:58:01 +0800

    feat: init traj_opt file

---
+++ fast_legged_planner_py/swing_leg_planner/traj_opt/traj_opt.py
@@ -0,0 +1,12 @@
+'''
+Author: RaymonYip-NUC11 2205929492@qq.com
+Date: 2023-11-13 10:01:31
+LastEditors: RaymonYip-NUC11
+LastEditTime: 2023-11-13 10:01:39
+FilePath: /flplanner_ws/src/fast_legged_planner/fast_legged_planner_py/swing_leg_planner/traj_opt/traj_opt.py
+Description: file content
+'''
+#!/usr/bin/env python
+# coding=utf-8
+
+from scipy.optimize import fmin_bfgs, fmin_l_bfgs_b
