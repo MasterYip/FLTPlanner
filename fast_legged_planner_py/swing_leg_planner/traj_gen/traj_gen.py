3afd77c8c2021f3a5d773a28404ec94de89ac648
RaymonYip-NUC11 <2205929492@qq.com>
2023-11-21 17:51:25 +0800

    feat: add UniBSpline

--- fast_legged_planner_py/swing_leg_planner/traj_gen/traj_gen.py
+++ fast_legged_planner_py/swing_leg_planner/traj_gen/traj_gen.py
@@ -2,14 +2,15 @@
 Author: RaymonYip-NUC11 2205929492@qq.com
 Date: 2023-11-02 17:56:55
 LastEditors: RaymonYip-NUC11
-LastEditTime: 2023-11-21 16:39:00
+LastEditTime: 2023-11-21 17:48:16
 FilePath: //flplanner_ws//src//fast_legged_planner//fast_legged_planner_py//swing_leg_planner//traj_gen//traj_gen.py
 Description: file content
 '''
 # -*- coding: utf-8 -*-
-from typing_extensions import deprecated
+from typing_extensions import deprecated, override
 from scipy.interpolate import BSpline, CubicSpline
 import numpy as np
+from scipy import interpolate
 from abc import abstractmethod, ABCMeta
 
 # Parameter Mat
@@ -64,9 +65,8 @@ def cubic_evaluate(knots: np.ndarray, t, para_mat: np.ndarray, eval_mode="pos"):
     else:
         return point
 
-# Not used
 
-
+@deprecated("Not used")
 def bezier_evaluate(knots, t):
     """
     Evaluate bezier curve at parameter t
@@ -119,7 +119,7 @@ class SplineBase(object):
     def set(self, params: np.ndarray):
         raise NotImplementedError(
             "Subclasses must implement the insert method")
-        
+
     @abstractmethod
     def get(self):
         raise NotImplementedError(
@@ -131,6 +131,10 @@ class SplineBase(object):
 
 
 class HermiteSpline(SplineBase):
+    """
+    Hermite spline class
+    """
+
     def __init__(self, params: np.ndarray):
         """Hermit spline initialization
         :param params: control points, literally `np.ndarray([p0, v0, p1, v1,..., pn, vn])`
@@ -138,14 +142,17 @@ class HermiteSpline(SplineBase):
         self.set(params)
         self.para_mat = HERMITE_MAT
 
+    @override
     def set(self, params: np.ndarray):
         self.params = params
         self.n = params.shape[0]  # Hermite case: pos poitns = n/2
         self.t_range = [0, self.n//2-1]
-    
+
+    @override
     def get(self):
         return self.params
 
+    @override
     def evaluate(self, t, eval_mode="pos"):
         # TODO: implement t_vec input
         if t < self.t_range[0] or t > self.t_range[1]:
@@ -161,6 +168,7 @@ class HermiteSpline(SplineBase):
         """
         return self.evaluate(t_norm*(self.t_range[1]-self.t_range[0])+self.t_range[0])
 
+    @override
     @deprecated("Insert will change the spline")
     def insert(self, t_list: list):
         """Insert knot at normalized parameter t
@@ -210,3 +218,33 @@ class HermiteSpline(SplineBase):
 
     def get_vellist(self):
         return self.params[1::2]
+
+
+class UniBSpline(SplineBase):
+    def __init__(self, params: np.ndarray, k=3):
+        """Uniform B-Spline initialization
+        TODO: Note: Start at p0 end at pn
+        :param params: control points, literally `np.ndarray([p0, p1, p2, p3,..., pn])`
+        """
+        self._k = k
+        self.set(params)
+        # self.para_mat = UNI_B_MAT
+
+    @override
+    def set(self, params: np.ndarray):
+        self.params = params
+        self.n = params.shape[0]
+        self.t_range = [0, self.n-1]
+        self._t = np.concatenate((np.zeros(self._k), np.arange(
+            self.n), np.ones(self._k)*self.t_range[1]))
+        dimen = params.shape[1]
+        self.bspline = interpolate.BSpline(self._t, np.concatenate(
+            (self.params[0].reshape(1, dimen), self.params, self.params[-1].reshape(1, dimen))), self._k)
+
+    @override
+    def get(self):
+        return self.params
+
+    @override
+    def evaluate(self, t, d_order: int = 0, normalized: bool = False):
+        return self.bspline(t)
