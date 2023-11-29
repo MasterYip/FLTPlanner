9cf966094879f39996521b17811376aad673e566
RaymonYip-NUC11 <2205929492@qq.com>
2023-11-29 18:56:31 +0800

    feat: IKFast - support python bind module renaming

--- src/ikfast/ik_bindings.cpp
+++ src/ikfast/ik_bindings.cpp
@@ -4,7 +4,7 @@
 namespace py = pybind11;
 
 #include "ikfast_interface.hpp"
-
+#include "config.h"
 
 // Bind the function to Python using pybind11
 void init_core(py::module m)
@@ -15,7 +15,7 @@ void init_core(py::module m)
     m.def("IKFast_trans3D", py::overload_cast<const std::vector<double>>(&IKFast_trans3D), py::arg("trans"), "Compute IK for 3D translation");
 }
 
-PYBIND11_MODULE(pyikfast, m)
+PYBIND11_MODULE(PYBIND_NAME, m)
 {
     init_core(m);
 }
