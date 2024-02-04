fcfa408a61ca5bd41f06b7df7b4de0ec6b5616dc
RaymonYip-NUC11 <2205929492@qq.com>
2024-02-04 14:53:37 +0800

    test: gridmap_interface

--- fast_legged_planner/fast_legged_planner_cpp/include/fast_legged_planner/perception_interface/GridmapInterface.h
+++ fast_legged_planner/fast_legged_planner_cpp/include/fast_legged_planner/perception_interface/GridmapInterface.h
@@ -1,12 +1,12 @@
 /**
  * @file GridmapInterface.h
  * @author Master Yip (2205929492@qq.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2024-02-04
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 #pragma once
 
@@ -20,59 +20,68 @@
 /* external project header files */
 #include <ros/ros.h>
 #include <grid_map_msgs/GridMap.h>
+#include <grid_map_ros/grid_map_ros.hpp>
 #include <grid_map_core/grid_map_core.hpp>
 #include <grid_map_sdf/SignedDistanceField.hpp>
 
 /* internal project header files */
 
+using Derivative3 = Eigen::Vector3d;
 class GridMapInterface
 {
 private:
     ros::NodeHandle nh;
     ros::Subscriber sub;
-    grid_map::GridMap grid_map;
+    grid_map::GridMap map_;
     std::unique_ptr<grid_map::SignedDistanceField> sdf[2];
+    std::pair<Eigen::Vector3d, Eigen::Vector3d> sdf_range[2];
     std::string ground_layer = "elevation";
     std::string ceiling_layer = "ceiling";
 
 public:
-    GridMapInterface(const std::string &topic_name = "grid_map")
+    GridMapInterface(const std::string &topic_name = "grid_map") : nh("~")
     {
         sub = nh.subscribe(topic_name, 1, &GridMapInterface::callback, this);
-        grid_map.setFrameId("map");
-        sdf[0] = sdf[1] = nullptr;
+        map_.setFrameId("map");
+        nh.param("elevation_layer", ground_layer, ground_layer);
+        nh.param("ceiling_layer", ceiling_layer, ceiling_layer);
+        // NOTE: update in blocking mode first
+        update();
     }
 
     void callback(const grid_map_msgs::GridMap &msg)
     {
-        grid_map = grid_map::GridMap(msg);
+        grid_map::GridMapRosConverter::fromMessage(msg, map_);
         if (!sdf[0])
-        {
             update();
-        }
     }
 
     void update(bool block = true, double sdf_margin = 0.2)
     {
-        while (grid_map.getLayers().empty() && block)
+        while (map_.getLayers().empty() && block && ros::ok())
         {
-            ROS_WARN("GridMap_Interface - GridMap is not subscribed!");
-            ros::Duration(0.5).sleep();
+            ROS_WARN("GridMap_Interface - Waiting for GridMap message...");
+            ros::spinOnce();
+            ros::Duration(2).sleep();
         }
         updateSDF(ground_layer, 0, sdf_margin);
-        if (grid_map.exists(ceiling_layer))
+        if (map_.exists(ceiling_layer))
         {
             updateSDF(ceiling_layer, 1, sdf_margin);
         }
     }
 
-    void updateSDF(const std::string &layer_name, size_t index, double margin = 0.2)
+    void updateSDF(const std::string &layer_name, uint index = 0, double margin = 0.2)
     {
         try
         {
-            grid_map::SignedDistanceField sdf_temp(grid_map, layer_name, margin);
+            auto &elevationData = map_.get(layer_name);
+            const double minValue{elevationData.minCoeffOfFinites() - margin};
+            const double maxValue{elevationData.maxCoeffOfFinites() + margin};
+            grid_map::SignedDistanceField sdf_temp(map_, layer_name, minValue, maxValue);
+            sdf_range[index] = std::make_pair(Eigen::Vector3d(0, 0, minValue),
+                                              Eigen::Vector3d(map_.getLength().x(), map_.getLength().y(), maxValue));
             sdf[index] = std::make_unique<grid_map::SignedDistanceField>(sdf_temp);
-            // Update sdf_range[index]
         }
         catch (const std::out_of_range &e)
         {
@@ -83,13 +92,12 @@ public:
     double value(const grid_map::Position &position, const std::string &layer_name = "")
     {
         if (layer_name.empty())
-        {
-            layer_name = ground_layer;
-        }
-        return grid_map.atPosition(layer_name, position);
+            return map_.atPosition(ground_layer, position);
+        else
+            return map_.atPosition(layer_name, position);
     }
 
-    double sdfValue(const grid_map::Position &position, size_t index = 0, const std::string &mode = "min")
+    double sdfValue(const grid_map::Position3 &position, size_t index = 0, const std::string &mode = "min")
     {
         if (mode == "min") // min of ground and ceiling
         {
@@ -101,11 +109,11 @@ public:
             else if (!sdf[1])
             {
                 ROS_WARN("Ceiling SDF is not initialized!");
-                return sdf[0]->getValue(position);
+                return sdf[0]->value(position);
             }
             else
             {
-                return std::min(sdf[0]->getValue(position), -sdf[1]->getValue(position));
+                return std::min(sdf[0]->value(position), -sdf[1]->value(position));
             }
         }
         else if (mode == "ground") // ground only
@@ -115,7 +123,7 @@ public:
                 ROS_WARN("SDF is not initialized!");
                 return 0.0;
             }
-            return sdf[index]->getValue(position);
+            return sdf[index]->value(position);
         }
         else
         {
@@ -123,21 +131,18 @@ public:
         }
     }
 
-    Eigen::Vector3d sdfDerivative(const grid_map::Position &position, size_t index = 0)
+    Derivative3 sdfDerivative(const grid_map::Position3 &position, size_t index = 0)
     {
         if (!sdf[index])
         {
             ROS_WARN("SDF is not initialized!");
             return Eigen::Vector3d::Zero();
         }
-        return sdf[index]->getGradient(position).transpose();
+        return sdf[index]->derivative(position).transpose();
     }
 
     grid_map::Length getRange() const
     {
-        return grid_map.getLength();
+        return map_.getLength();
     }
-
 };
-
-
--- fast_legged_planner/fast_legged_planner_cpp/test/perception_interface/test_gridmap_interface.cpp
+++ fast_legged_planner/fast_legged_planner_cpp/test/perception_interface/test_gridmap_interface.cpp
@@ -12,8 +12,22 @@
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "grid_map_interface_cpp");
-    GridMapInterface interface;
-    ros::spin();
+    ros::init(argc, argv, "test_gridmap_interface");
+    ros::NodeHandle nh("~");
+    ros::Rate rate(1);
+    GridMapInterface interface("/grid_map");
+    grid_map::Length range = interface.getRange();
+    std::cout << "Range: " << range[0] << " " << range[1] << std::endl;
+    while (ros::ok())
+    {
+        grid_map::Position pos(range[0]/2, range[1]/2);
+        double height = interface.value(pos);
+        std::cout << "Height at center: " << height << std::endl;
+        grid_map::Position3 pos3(pos.x(), pos.y(), height);
+        double sdf_value = interface.sdfValue(pos3);
+        std::cout << "SDF at center: " << sdf_value << std::endl;
+        rate.sleep();
+        ros::spinOnce();
+    }
     return 0;
 }
