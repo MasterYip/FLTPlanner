bdd25c4d30913e88d85dd336fc8dbce140f1c068
HexLab-NUC12-MasterYip <2205929492@qq.com>
2024-03-18 21:49:56 +0800

    feat: add force compansation planner

--- fast_legged_planner/fast_legged_planner_cpp/src/robot_interface/ElSpiderAirInterfaceROS.cpp
+++ fast_legged_planner/fast_legged_planner_cpp/src/robot_interface/ElSpiderAirInterfaceROS.cpp
@@ -58,6 +58,42 @@ void ElSpiderAirInterfaceROS::pub_footcmd_from_footendpos(const std::vector<Eige
     foot_pos_pub.publish(footcmd);
 }
 
+void ElSpiderAirInterfaceROS::pub_footcmd_from_footendcmd(const std::vector<Eigen::Vector3d> &footendpos,
+                                                          const std::vector<Eigen::Vector3d> &footendvel,
+                                                          const std::vector<Eigen::Vector3d> &footendeffort)
+{
+    fast_legged_planner::FootCmd footcmd;
+    footcmd.header.stamp = ros::Time::now();
+    footcmd.feedforward_type = feedforward_type; // Default 0, foot feedforward
+    for (int i = 0; i < 6; ++i)
+    {
+        geometry_msgs::Point pt;
+        pt.x = footendpos[i][0];
+        pt.y = footendpos[i][1];
+        pt.z = footendpos[i][2];
+        footcmd.foot_position.push_back(pt);
+        geometry_msgs::Vector3 vec3;
+        footcmd.joint_torque.push_back(vec3); // zero, not used
+        vec3.x = footendvel[i][0];
+        vec3.y = footendvel[i][1];
+        vec3.z = footendvel[i][2];
+        footcmd.foot_velocity.push_back(vec3);
+        vec3.x = footendeffort[i][0];
+        vec3.y = footendeffort[i][1];
+        vec3.z = footendeffort[i][2];
+        footcmd.foot_effort.push_back(vec3);
+        vec3.x = joint_kp[0];
+        vec3.y = joint_kp[1];
+        vec3.z = joint_kp[2];
+        footcmd.joint_kp.push_back(vec3);
+        vec3.x = joint_kd[0];
+        vec3.y = joint_kd[1];
+        vec3.z = joint_kd[2];
+        footcmd.joint_kd.push_back(vec3);
+    }
+    foot_pos_pub.publish(footcmd);
+}
+
 void ElSpiderAirInterfaceROS::pub_odom(const pinocchio::SE3 &odom,
                                        const std::string &child_frame,
                                        const std::string &parent_frame)
---
+++ fast_legged_planner/src/elspider_air_force_ctrl_test.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file elspider_air_force_ctrl_test.cpp
+ * @author Master Yip (2205929492@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-03-18
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "fast_legged_planner/elspider_air_planner/ElSpiderAirSimplePlanner.h"
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "elspider_air_swingtraj_planner");
+    ros::Time::init(); // FIXME: some where call ros::Time::now() before nh_ initialized
+    bool fake_feedback = ros::param::param<bool>("~fake_feedback", false);
+    bool simulation = ros::param::param<bool>("~sim", false);
+    ElSpiderAirSimplePlanner planner(fake_feedback, simulation);
+    planner.run();
+    return 0;
+}
