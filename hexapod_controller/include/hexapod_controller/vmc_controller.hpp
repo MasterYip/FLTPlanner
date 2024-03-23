6ec49ed10ad9a4498a907ea79d414753f41af672
HexLab-NUC12-MasterYip <2205929492@qq.com>
2024-03-23 17:23:58 +0800

    feat: update vmc controller

--- hexapod_controller/include/hexapod_controller/vmc_controller.hpp
+++ hexapod_controller/include/hexapod_controller/vmc_controller.hpp
@@ -25,28 +25,79 @@
 #include <Eigen/Core>
 
 #include <ros/ros.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
 /* internal project header files */
+
+struct VMCConfig
+{
+    double mu;
+    double mass;
+    double gravity;
+    double loop_rate;
+
+    std::string exp_pose_topic_name;
+    std::string exp_foot_state_topic_name;
+    std::string fdb_foot_state_topic_name;
+    std::string footcmd_topic_name;
+    std::string body_frame;
+    std::string world_frame;
+
+    inline void
+    loadParameters(const ros::NodeHandle &nh)
+    {
+        nh.param("mu", mu, 0.5);
+        nh.param("mass", mass, 30.0);
+        nh.param("gravity", gravity, 9.81);
+        nh.param("loop_rate", loop_rate, 200.0);
+        nh.param("exp_pose_topic_name", exp_pose_topic_name, std::string("/exp_pose"));
+        nh.param("exp_foot_state_topic_name", exp_foot_state_topic_name, std::string("/exp_foot_state"));
+        nh.param("fdb_foot_state_topic_name", fdb_foot_state_topic_name, std::string("/fdb_foot_state"));
+        nh.param("footcmd_topic_name", footcmd_topic_name, std::string("/footcmd_track"));
+        nh.param("body_frame", body_frame, std::string("base"));
+        nh.param("world_frame", world_frame, std::string("odom"));
+        return;
+    };
+};
 
 class VMCController
 {
 
 private:
-    double mass_;
-    double gravity_ = 9.8;
-    double mu;
-    pinocchio::SE3 com_pos_;
+    VMCConfig cfg_;
 
-    pinocchio::SE3 exp_pos_;
+    // State Expected
+    // pose
+    pinocchio::SE3 exp_pose_;
+    ros::Subscriber exp_pose_sub_;
+    bool recv_exp_pose_ = false;
+    // foot_pos
+    hexapod_controller::FootState exp_foot_state_;
+    ros::Subscriber exp_foot_state_sub_;
+    bool recv_exp_foot_state_ = false;
 
-    FootCmd foot_cmd_;
-    ros::Subscriber foot_cmd_pub_;
-    FootState foot_state_;
-    ros::Publisher foot_state_sub_;
+    // State Feedback
+    // pose
+    tf2_ros::Buffer tfBuffer_;
+    tf2_ros::TransformListener tfListener_;
+    geometry_msgs::TransformStamped body_state_tf_;
+    pinocchio::SE3 fdb_pose_; // Feedback
+    bool recv_fdb_pose_ = false;
+    // foot_pos
+    hexapod_controller::FootState fdb_foot_state_;
+    ros::Subscriber fdb_foot_state_sub_;
+    bool recv_fdb_foot_state_ = false;
 
+    // Control Commands
+    hexapod_controller::FootCmd foot_cmd_;
+    ros::Publisher foot_cmd_pub_;
 
 public:
     VMCController(ros::NodeHandle &nh);
-    void footStateCallback(const hexapod_controller::FootState &msg);
+    bool fdbPoseLookup();
+    void expPoseCallback(const geometry_msgs::TransformStamped &msg);
+    void fdbFootStateCallback(const hexapod_controller::FootState &msg);
+    void expFootStateCallback(const hexapod_controller::FootState &msg);
     bool getExpWrench(const pinocchio::SE3 &com_pos,
                       const pinocchio::SE3 &exp_pos,
                       pinocchio::Force &exp_wrench);
@@ -54,4 +105,5 @@ public:
                                 const std::vector<Eigen::Vector3d> foot_pos,
                                 std::vector<Eigen::Vector3d> &grf);
     void controllLoop();
+    void run();
 };
--- hexapod_controller/src/vmc_controller.cpp
+++ hexapod_controller/src/vmc_controller.cpp
@@ -1,22 +1,87 @@
 /**
  * @file vmc_controller.cpp
  * @author Master Yip (2205929492@qq.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2024-03-22
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 
 #include "hexapod_controller/vmc_controller.hpp"
 
-VMCController::VMCController(ros::NodeHandle &nh)
+pinocchio::SE3 transformToSE3(const geometry_msgs::TransformStamped &tf)
 {
-    foot_cmd_sub_ = nh.subscribe("/foot_state_sub", 1, &VMCController::footStateCallback, this);
+    return pinocchio::SE3(Eigen::Quaterniond(tf.transform.rotation.w, tf.transform.rotation.x,
+                                             tf.transform.rotation.y, tf.transform.rotation.z),
+                          Eigen::Vector3d(tf.transform.translation.x, tf.transform.translation.y, tf.transform.translation.z));
 }
 
-VMCController::footStateCallback(const hexapod_controller::FootState &msg)
+VMCController::VMCController(ros::NodeHandle &nh) : tfListener_(tfBuffer_)
 {
-    foot_state_ = msg;
+    cfg_.loadParameters(nh);
+    exp_foot_state_sub_ = nh.subscribe(cfg_.exp_foot_state_topic_name, 1, &VMCController::expFootStateCallback, this);
+    fdb_foot_state_sub_ = nh.subscribe(cfg_.fdb_foot_state_topic_name, 1, &VMCController::fdbFootStateCallback, this);
+    exp_pose_sub_ = nh.subscribe(cfg_.exp_pose_topic_name, 1, &VMCController::expPoseCallback, this);
+    foot_cmd_pub_ = nh.advertise<hexapod_controller::FootCmd>(cfg_.footcmd_topic_name, 1);
+}
+
+bool VMCController::fdbPoseLookup()
+{
+    try
+    {
+        // FIXME: Make sure it is updated in real-time
+        body_state_tf_ = tfBuffer_.lookupTransform(cfg_.world_frame, cfg_.body_frame, ros::Time(0));
+    }
+    catch (tf2::TransformException &ex)
+    {
+        ROS_WARN("Could not get body state: %s", ex.what());
+        return false;
+    }
+    fdb_pose_ = transformToSE3(body_state_tf_);
+    recv_fdb_pose_ = true;
+    return true;
+}
+
+void VMCController::expPoseCallback(const geometry_msgs::TransformStamped &msg)
+{
+    exp_pose_ = transformToSE3(msg);
+    recv_exp_pose_ = true;
+}
+
+void VMCController::fdbFootStateCallback(const hexapod_controller::FootState &msg)
+{
+    fdb_foot_state_ = msg;
+    recv_fdb_foot_state_ = true;
+}
+
+void VMCController::expFootStateCallback(const hexapod_controller::FootState &msg)
+{
+    exp_foot_state_ = msg;
+    recv_exp_foot_state_ = true;
+}
+
+void VMCController::controllLoop()
+{
+    fdbPoseLookup();
+    if (recv_exp_pose_ && recv_exp_foot_state_ && recv_fdb_foot_state_ && recv_fdb_pose_)
+    {
+        // TODO
+    }
+    else
+    {
+        ROS_WARN("Not all states are received");
+    }
+}
+
+void VMCController::run()
+{
+    ros::Rate loop_rate(cfg_.loop_rate);
+    while (ros::ok())
+    {
+        // TODO
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
 }
