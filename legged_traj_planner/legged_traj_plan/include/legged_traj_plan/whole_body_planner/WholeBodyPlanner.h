ce86bea9dbc97fa978d43808cfebc79865d6e469
HexLab-NUC12-MasterYip <2205929492@qq.com>
2024-04-29 15:20:21 +0800

    add: raibert planner draft

--- legged_traj_planner/legged_traj_plan/include/legged_traj_plan/whole_body_planner/WholeBodyPlanner.h
+++ legged_traj_planner/legged_traj_plan/include/legged_traj_plan/whole_body_planner/WholeBodyPlanner.h
@@ -17,7 +17,7 @@
 /* c++ standard library header files */
 
 /* external project header files */
-
+#include <geometry_msgs/Twist.h>
 /* internal project header files */
 
 #include "legged_traj_plan/utils/CircleQueue.h"
@@ -49,3 +49,180 @@ public:
 
     // std::pair<std::vector<std::vector<double>>, std::vector<std::vector<double>>> get_foot_traj(double t, int point_num, double delta);
 };
+
+class LegSwitchScheduler
+{
+private:
+    double interval_;
+    double duty_;
+    double phase_shift_;
+
+    double start_time_ = 0;
+    bool is_running_ = false;
+
+public:
+    // FIXME: add phase
+    /**
+     * @brief Construct a new Leg Switch Scheduler object
+     *
+     * @param interval Interval of the scheduler (sec)
+     * @param duty Duty (0~1)
+     * @param phase_shift_ Phase shift (0~1) - apply DELAY to the scheduler
+     */
+    LegSwitchScheduler(double interval, double duty, double phase_shift = 0)
+        : interval_(interval), duty_(duty), phase_shift_(phase_shift){};
+
+    /**
+     * @brief Reset scheduler
+     * @note [___stance___```swing```|___stance___```swing```]
+     * @param start_time
+     */
+    void reset(double start_time)
+    {
+        start_time_ = start_time;
+        is_running_ = true;
+    };
+
+    /**
+     * @brief Query the support state at time t
+     *
+     * @param t
+     * @return true Stance
+     * @return false Swing
+     */
+    bool querySuppotState(double t)
+    {
+        if (!is_running_)
+            return true;
+        double t_local = t - start_time_ - phase_shift_ * interval_;
+        if (t_local < 0)
+            return true;
+
+        double t_local_mod = fmod(t_local, interval_);
+        if (t_local_mod < duty_ * interval_)
+            return true;
+        else
+            return false;
+    }
+
+    bool getSucceedingSwitchTimePair(double t, &double t_lift, &double t_touch,
+                                     uint succeed_num = 0)
+    {
+        if (!is_running_)
+            return false;
+        double t_local = t - start_time_ - phase_shift_ * interval_;
+        if (t_local < 0)
+            return false;
+        double t_local_mod = fmod(t_local, interval_);
+        if (t_local_mod < duty_ * interval_)
+        {
+            t_lift = (t - t_local_mod + duty_ * interval_) + interval_ * succeed_num;
+            t_touch = (t - t_local_mod + interval_) + interval_ * succeed_num;
+            return true;
+        }
+        else
+        {
+            t_lift = (t - t_local_mod + duty_ * interval_ + interval_) + interval_ * succeed_num;
+            t_touch = (t - t_local_mod + interval_ + interval_) + interval_ * succeed_num;
+            return true;
+        }
+    }
+}
+
+// FIXME: temporarily use workspace traj
+class LegTrajSet
+{
+private:
+    std::vector<Eigen::Vector3d> foothold_;
+    std::vector<std::shared_ptr<TrajectoryBase>> swing_traj_;
+
+public:
+    LegTrajSet() = default;
+
+    setInit(Eigen::Vector3d foothold){
+        foothold_.clear();
+        foothold_.emplace_back(foothold);
+    };
+
+    void appendStepTraj(Eigen::Vector3d foothold, std::shared_ptr<TrajectoryBase> swing_traj)
+    {
+        swing_traj_.emplace_back(swing_traj);
+        foothold_.emplace_back(foothold);
+    }
+
+    void popStepTraj()
+    {
+        swing_traj_.pop_front();
+        foothold_.pop_front();
+    }
+
+    Eigen::Vector3d getLastFoothold()
+    {
+        return foothold_.back();
+    }
+}
+
+class CmdVelExtraplator
+{
+private:
+    geometry_msgs::Twist cmd_vel_; // cmd vel relative to the BASE frame
+    pinocchio::SE3 pose_;
+
+public:
+    CmdVelExtraplator() = default;
+    void update(pinocchio::SE3 pose, geometry_msgs::Twist cmd_vel = geometry_msgs::Twist())
+    {
+        // FIXME: is default cmd_vel 0?
+        cmd_vel_ = cmd_vel;
+        pose_ = pose;
+    }
+
+    /**
+     * @brief Extrapolate the pose by dt
+     *
+     * @param dt Delta t
+     * @return pinocchio::SE3
+     */
+    pinocchio::SE3 extrapolate(double dt)
+    {
+        pinocchio::SE3 pose_new = pose_;
+        Eigen::Vector3d linear_world = pose_.rotation().transpose() * cmd_vel_.linear;
+        Eigen::Vector3d angular_world = pose_.rotation().transpose() * cmd_vel_.angular;
+        pose_new.translation() += linear_world * dt;
+        pose_new.rotation() = pose_.rotation() * pinocchio::SE3::exp(angular_world * dt).rotation();
+        return pose_new;
+    }
+
+}
+
+// class RaibertHeuristicSelector
+// {
+// private:
+
+// }
+
+class RaibertHeuristicPlanner
+{
+private:
+    double interval_ = 1.0;
+    double duty_ = 0.5;
+
+    PosList nominal_foothold_base_;
+    std::shared_ptr<ElSpiderAirInterface> robot_interface_;
+    std::shared_ptr<GridMapInterface> gridmap_interface_;
+    std::shared_ptr<SwingTrajPlanner> swing_traj_planner_;
+    CmdVelExtraplator cmd_vel_extraplator_;
+    std::vector<LegSwitchScheduler> switch_scheduler_;
+    std::vector<LegTrajSet> leg_traj_set_(6);
+    double update_time_ = 0;
+    bool use_cfg_space_;
+
+public:
+    RaibertHeuristicPlanner(SwingTrajPlannerConfig swing_traj_planner_config,
+                            std::shared_ptr<GridMapInterface> gridmap_interface,
+                            std::shared_ptr<ElSpiderAirInterface> robot_interface);
+
+    void start(pinocchio::SE3 pose, PosList foot_pos_list);
+
+    void update(pinocchio::SE3 pose, geometry_msgs::Twist cmd_vel);
+}
--- legged_traj_planner/legged_traj_plan/src/whole_body_planner/WholeBodyPlanner.cpp
+++ legged_traj_planner/legged_traj_plan/src/whole_body_planner/WholeBodyPlanner.cpp
@@ -62,3 +62,90 @@ int MCTSWholeBodyPlanner::get_state_traj_length()
 //     }
 //     return std::make_pair(default_traj_list, opt_traj_list);
 // }
+
+RaibertHeuristicPlanner::RaibertHeuristicPlanner(SwingTrajPlannerConfig swing_traj_planner_config,
+                                                 std::shared_ptr<GridMapInterface> gridmap_interface,
+                                                 std::shared_ptr<ElSpiderAirInterface> robot_interface)
+    : gridmap_interface_(gridmap_interface), robot_interface_(robot_interface),
+      swing_traj_planner_(std::make_shared<SwingTrajPlanner>(swing_traj_planner_config, robot_interface_, gridmap_interface_)),
+      use_cfg_space_(swing_traj_planner_config.useCfgSpace)
+{
+    switch_scheduler_.emplace_back(LegSwitchScheduler(interval_, duty_, 0));
+    switch_scheduler_.emplace_back(LegSwitchScheduler(interval_, duty_, 0.5));
+    switch_scheduler_.emplace_back(LegSwitchScheduler(interval_, duty_, 0));
+    switch_scheduler_.emplace_back(LegSwitchScheduler(interval_, duty_, 0.5));
+    switch_scheduler_.emplace_back(LegSwitchScheduler(interval_, duty_, 0));
+    switch_scheduler_.emplace_back(LegSwitchScheduler(interval_, duty_, 0.5));
+    nominal_foothold_base_.emplace_back(Eigen::Vector3d(0.354, -0.28, -0.28));
+    nominal_foothold_base_.emplace_back(Eigen::Vector3d(0.054, -0.34, -0.28));
+    nominal_foothold_base_.emplace_back(Eigen::Vector3d(-0.354, -0.28, -0.28));
+    nominal_foothold_base_.emplace_back(Eigen::Vector3d(0.354, 0.28, -0.28));
+    nominal_foothold_base_.emplace_back(Eigen::Vector3d(0.054, 0.34, -0.28));
+    nominal_foothold_base_.emplace_back(Eigen::Vector3d(-0.354, 0.28, -0.28));
+}
+
+void RaibertHeuristicPlanner::start(pinocchio::SE3 pose, PosList foot_pos_list)
+{
+
+    // FIXME: temp
+    for (auto &leg_sch : switch_scheduler_)
+    {
+        leg_sch.reset(ros::Time::now().toSec());
+    }
+    cmd_vel_extraplator_.update(pose);
+    update_time_ = ros::Time::now().toSec();
+    for (int i = 0; i < 6; ++i)
+    {
+        leg_traj_set_[i].setInit(foot_pos_list[i]);
+    }
+}
+
+void RaibertHeuristicPlanner::update(pinocchio::SE3 pose, geometry_msgs::Twist cmd_vel)
+{
+    cmd_vel_extraplator_.update(pose, cmd_vel);
+    update_time_ = ros::Time::now().toSec();
+    preCompute();
+}
+
+void RaibertHeuristicPlanner::preCompute(uint swing_traj_num = 1)
+{
+
+    double t_lift1, t_down1, t_lift2, t_down2;
+    pinocchio::SE3 pose_lift1, pose_mid1, pose_down1, pose_lift2, pose_mid2, pose_down2;
+    switch_scheduler_[0].getSucceedingSwitchTimePair(ros::Time::now().toSec(), t_lift1, t_down1);
+    switch_scheduler_[1].getSucceedingSwitchTimePair(ros::Time::now().toSec(), t_lift2, t_down2);
+    // pose_lift1 = cmd_vel_extraplator_.extrapolate(t_lift1 - update_time_);
+    // pose_down1 = cmd_vel_extraplator_.extrapolate(t_down1 - update_time_);
+    // pose_lift2 = cmd_vel_extraplator_.extrapolate(t_lift2 - update_time_);
+    // pose_down2 = cmd_vel_extraplator_.extrapolate(t_down2 - update_time_);
+    pose_mid1 = cmd_vel_extraplator_.extrapolate((t_lift1 + t_down1) / 2 - update_time_);
+    pose_mid2 = cmd_vel_extraplator_.extrapolate((t_lift2 + t_down2) / 2 - update_time_);
+
+    for (uint i = 0; i < 6; i += 2)
+    {
+        Eigen::Vector3d p0 = leg_traj_set_[i].getLastFoothold();
+        Eigen::Vector3d p1 = point_SE3Act(pose_mid1, nominal_foothold_base_[i]);
+        leg_traj_set_[i].appendStepTraj(p1, swing_traj_planner_->getDefaultTraj(p0, p1, 0.2, 0.1));
+    }
+    for (uint i = 1; i < 6; i += 2)
+    {
+        Eigen::Vector3d p0 = leg_traj_set_[i].getLastFoothold();
+        Eigen::Vector3d p1 = point_SE3Act(pose_mid2, nominal_foothold_base_[i]);
+        leg_traj_set_[i].appendStepTraj(p1, swing_traj_planner_->getDefaultTraj(p0, p1, 0.2, 0.1));
+    }
+
+    // for (int i = 0; i < 6; ++i)
+    // {
+    //     // swingtraj_[i].emplace_back(swing_traj_planner_->getCfgInitTraj(
+    // }
+}
+
+bool RaibertHeuristicPlanner::query(double t, PosList &foot_pos_list)
+{
+    pose = cmd_vel_extraplator_.extrapolate(t);
+    for (int i = 0; i < 6; ++i)
+    {
+        foot_pos_list[i] = swing_traj_planner_->query(pose, i);
+    }
+    return true;
+}
