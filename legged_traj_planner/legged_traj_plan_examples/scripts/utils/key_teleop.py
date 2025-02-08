244574cc5626718f07c5c94c63e1c5e7d8048a15
HexLab-NUC12-MasterYip <2205929492@qq.com>
2025-02-08 17:27:52 +0800

    feat: add new key_teleop

---
+++ legged_traj_planner/legged_traj_plan_examples/scripts/utils/key_teleop.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+import sys
+import select
+import tty
+import termios
+
+
+class KeyTeleop:
+    def __init__(self):
+        rospy.init_node('key_teleop')
+        # Default value
+        cmd_topic = 'cmd_vel'
+        self.pub_rate = 10.0  # Adjust as needed
+        self.max_linear_x = 0.4  # Adjust as needed
+        self.max_linear_y = 0.2  # Adjust as needed
+        self.max_angular_z = 0.4  # Adjust as needed
+        # Ros param
+        cmd_topic = rospy.get_param('~cmd_topic', cmd_topic)
+        self.pub_rate = rospy.get_param('~pub_rate', self.pub_rate)
+        self.max_linear_x = rospy.get_param('~max_linear_x', self.max_linear_x)
+        self.max_linear_y = rospy.get_param('~max_linear_y', self.max_linear_y)
+        self.max_angular_z = rospy.get_param('~max_angular_z', self.max_angular_z)
+
+        self.timer = rospy.Timer(rospy.Duration(1.0/self.pub_rate), self.timer_callback)
+        self.cmd_vel_pub = rospy.Publisher(cmd_topic, Twist, queue_size=10)
+        self.twist_cmd = Twist()
+
+    def timer_callback(self, event):
+        self.update_cmd()
+        self.cmd_vel_pub.publish(self.twist_cmd)
+
+    def run(self):
+        rospy.spin()
+    
+    def read_key(self):
+        tty.setcbreak(sys.stdin.fileno())
+        rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+        if rlist:
+            key = sys.stdin.read(1)
+            return key
+        return None
+
+    def update_cmd(self):
+        key = self.read_key()
+        if key == 'w':
+            self.twist_cmd.linear.x = self.max_linear_x
+        elif key == 's':
+            self.twist_cmd.linear.x = -self.max_linear_x
+        elif key == 'a':
+            self.twist_cmd.linear.y = self.max_linear_y
+        elif key == 'd':
+            self.twist_cmd.linear.y = -self.max_linear_y
+        elif key == 'q':
+            self.twist_cmd.angular.z = self.max_angular_z
+        elif key == 'e':
+            self.twist_cmd.angular.z = -self.max_angular_z
+        # ctrl + c
+        elif key == '\x03':
+            rospy.signal_shutdown('shutdown')
+
+
+if __name__ == '__main__':
+    key_teleop = KeyTeleop()
+    key_teleop.run()
